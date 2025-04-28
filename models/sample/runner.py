0e91f3889ea1324a40928c38513d5357490f4f63
hirakawa <hirakawa2022@habe-lab.org>
2025-04-28 14:06:54 +0900

    feat: sample_vitの学習時間を計測可能に

--- models/sample/runner.py
+++ models/sample/runner.py
@@ -1,10 +1,11 @@
 import time
+from models.sample.train_vit import train_vit
 
 """時間計測のみをテストするサンプルコード
 """
 
 def train(epochs=10):
-    time.sleep(10)
+    train_vit(epochs=epochs)
 
 def infer():
     time.sleep(5)
